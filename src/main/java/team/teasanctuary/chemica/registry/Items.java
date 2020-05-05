67f033b93d768f9062205a1b7a1c111218e27fdf
TrueMemer <mkoaleksedos@gmail.com>
2020-05-05 14:44:35 +0300

    Fixed creative tabs and materials registration

--- src/main/java/team/teasanctuary/chemica/registry/Items.java
+++ src/main/java/team/teasanctuary/chemica/registry/Items.java
@@ -1,6 +1,10 @@
 package team.teasanctuary.chemica.registry;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import team.teasanctuary.chemica.items.BatteryItem;
 import team.teasanctuary.chemica.items.ScrewdriverItem;
@@ -12,10 +16,15 @@ public class Items {
     public static BatteryItem BATTERY_ITEM;
     public static TesterItem TESTER_ITEM;
 
+    public static final ItemGroup CHEMICA_ITEMS = FabricItemGroupBuilder.create(
+            new Identifier("chemica", "items"))
+            .icon(() -> new ItemStack(TESTER_ITEM))
+            .build();
+
     public static void init() {
-        SCREWDRIVER_ITEM = new ScrewdriverItem(new Item.Settings().maxCount(1));
-        BATTERY_ITEM = new BatteryItem(new Item.Settings().maxCount(1));
-        TESTER_ITEM = new TesterItem(new Item.Settings().maxCount(1));
+        SCREWDRIVER_ITEM = new ScrewdriverItem(new Item.Settings().maxCount(1).group(CHEMICA_ITEMS));
+        BATTERY_ITEM = new BatteryItem(new Item.Settings().maxCount(1).group(CHEMICA_ITEMS));
+        TESTER_ITEM = new TesterItem(new Item.Settings().maxCount(1).group(CHEMICA_ITEMS));
 
         register();
     }
--- src/main/java/team/teasanctuary/chemica/registry/Materials.java
+++ src/main/java/team/teasanctuary/chemica/registry/Materials.java
@@ -1,6 +1,9 @@
 package team.teasanctuary.chemica.registry;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -8,9 +11,15 @@ public class Materials {
 
     public static Item COPPER_INGOT;
 
+    public static final ItemGroup CHEMICA_MATERIALS = FabricItemGroupBuilder.create(
+            new Identifier("chemica", "materials"))
+            .icon(() -> new ItemStack(COPPER_INGOT))
+            .build();
 
     public static void init() {
-        COPPER_INGOT = new Item(new Item.Settings());
+        COPPER_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
+
+        register();
     }
 
     private static void register() {
