2b44b52bb4716b6e2711ce3b4d1f337d7cc6a4a8
Ivan Kuzmenko <6745157+rndtrash@users.noreply.github.com>
2020-10-03 19:00:30 +0300

    Still removing unplanned ores and blocks

--- src/main/java/team/teasanctuary/chemica/registry/Materials.java
+++ src/main/java/team/teasanctuary/chemica/registry/Materials.java
@@ -10,31 +10,14 @@ import net.minecraft.util.registry.Registry;
 public class Materials {
 
     public static Item COPPER_INGOT;
-    public static Item ALUMINUM_INGOT;
-    public static Item BRONZE_INGOT;
-    public static Item LEAD_INGOT;
-    public static Item MAGNESIUM_INGOT;
-    public static Item SILVER_INGOT;
-    public static Item STEEL_INGOT;
-    public static Item TIN_INGOT;
     public static Item ZINC_INGOT;
 
-    public static Item ARGENTITE_ORE_DUST;
-    public static Item CASSITERITE_ORE_DUST;
     public static Item COAL_ORE_DUST;
-    public static Item CUPRITE_ORE_DUST;
     public static Item DIAMOND_ORE_DUST;
-    public static Item GALENA_ORE_DUST;
     public static Item GOLD_ORE_DUST;
     public static Item IRON_ORE_DUST;
-    public static Item MALACHITE_ORE_DUST;
-    public static Item PYRITE_ORE_DUST;
     public static Item REDSTONE_ORE_DUST;
-    public static Item RUTILE_ORE_DUST;
-    public static Item SILVER_ORE_DUST;
     public static Item ZINCITE_ORE_DUST;
-    public static Item SPHALERITE_ORE_DUST;
-    public static Item CHALCOPYRITE_ORE_DUST;
 
     public static Item COKE_ITEM;
 
@@ -47,31 +30,14 @@ public class Materials {
 
     public static void init() {
         COPPER_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        ALUMINUM_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        BRONZE_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        LEAD_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        MAGNESIUM_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        SILVER_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        STEEL_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        TIN_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         ZINC_INGOT = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
 
-        ARGENTITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        CASSITERITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         COAL_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        CUPRITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         DIAMOND_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        GALENA_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         GOLD_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         IRON_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        MALACHITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        PYRITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         REDSTONE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        RUTILE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        SILVER_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
         ZINCITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        SPHALERITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
-        CHALCOPYRITE_ORE_DUST = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
 
         COKE_ITEM = new Item(new Item.Settings().group(CHEMICA_MATERIALS));
 
@@ -82,31 +48,14 @@ public class Materials {
 
     private static void register() {
         Registry.register(Registry.ITEM, new Identifier("chemica", "copper_ingot"), COPPER_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "aluminum_ingot"), ALUMINUM_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "bronze_ingot"), BRONZE_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "lead_ingot"), LEAD_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "magnesium_ingot"), MAGNESIUM_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "silver_ingot"), SILVER_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "steel_ingot"), STEEL_INGOT);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "tin_ingot"), TIN_INGOT);
         Registry.register(Registry.ITEM, new Identifier("chemica", "zinc_ingot"), ZINC_INGOT);
 
-        Registry.register(Registry.ITEM, new Identifier("chemica", "argentite_ore_dust"), ARGENTITE_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "cassiterite_ore_dust"), CASSITERITE_ORE_DUST);
         Registry.register(Registry.ITEM, new Identifier("chemica", "coal_ore_dust"), COAL_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "cuprite_ore_dust"), CUPRITE_ORE_DUST);
         Registry.register(Registry.ITEM, new Identifier("chemica", "diamond_ore_dust"), DIAMOND_ORE_DUST);
         Registry.register(Registry.ITEM, new Identifier("chemica", "gold_ore_dust"), GOLD_ORE_DUST);
         Registry.register(Registry.ITEM, new Identifier("chemica", "iron_ore_dust"), IRON_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "malachite_ore_dust"), MALACHITE_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "pyrite_ore_dust"), PYRITE_ORE_DUST);
         Registry.register(Registry.ITEM, new Identifier("chemica", "redstone_ore_dust"), REDSTONE_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "rutile_ore_dust"), RUTILE_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "silver_ore_dust"), SILVER_ORE_DUST);
         Registry.register(Registry.ITEM, new Identifier("chemica", "zincite_ore_dust"), ZINCITE_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "sphalerite_ore_dust"), SPHALERITE_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "galena_ore_dust"), GALENA_ORE_DUST);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "chalcopyrite_ore_dust"), CHALCOPYRITE_ORE_DUST);
 
         Registry.register(Registry.ITEM, new Identifier("chemica", "coke"), COKE_ITEM);
 
--- src/main/java/team/teasanctuary/chemica/registry/Ores.java
+++ src/main/java/team/teasanctuary/chemica/registry/Ores.java
@@ -11,74 +11,27 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 public class Ores {
 
-    public static Block MALACHITE_ORE_BLOCK;
-    public static Block ARGENTITE_ORE_BLOCK;
-    public static Block CHALCOPYRITE_ORE_BLOCK;
-    public static Block GALENA_ORE_BLOCK;
-    public static Block CUPRITE_ORE_BLOCK;
-    public static Block PYRITE_ORE_BLOCK;
-    public static Block RUTILE_ORE_BLOCK;
-    public static Block SILVER_ORE_BLOCK;
-    public static Block SPHALERITE_ORE_BLOCK;
     public static Block ZINCITE_ORE_BLOCK;
-    public static Block CASSITERITE_ORE_BLOCK;
+
+    // TODO: more ore features!
+    public static ConfiguredFeature<?, ?> ORE_ZINCITE;
 
     public static final ItemGroup CHEMICA_ORES = FabricItemGroupBuilder.create(
             new Identifier("chemica", "ores"))
-            .icon(() -> new ItemStack(MALACHITE_ORE_BLOCK))
+            .icon(() -> new ItemStack(ZINCITE_ORE_BLOCK))
             .build();
 
     private static void register() {
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "malachite_ore"), MALACHITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "malachite_ore"), new BlockItem(MALACHITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "argentite_ore"), ARGENTITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "argentite_ore"), new BlockItem(ARGENTITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "chalcopyrite_ore"), CHALCOPYRITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "chalcopyrite_ore"), new BlockItem(CHALCOPYRITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "galena_ore"), GALENA_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "galena_ore"), new BlockItem(GALENA_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "cuprite_ore"), CUPRITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "cuprite_ore"), new BlockItem(CUPRITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "rutile_ore"), RUTILE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "rutile_ore"), new BlockItem(RUTILE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "pyrite_ore"), PYRITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "pyrite_ore"), new BlockItem(PYRITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "silver_ore"), SILVER_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "silver_ore"), new BlockItem(SILVER_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "sphalerite_ore"), SPHALERITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "sphalerite_ore"), new BlockItem(SPHALERITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
         Registry.register(Registry.BLOCK, new Identifier("chemica", "zincite_ore"), ZINCITE_ORE_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("chemica", "zincite_ore"), new BlockItem(ZINCITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
-
-        Registry.register(Registry.BLOCK, new Identifier("chemica", "cassiterite_ore"), CASSITERITE_ORE_BLOCK);
-        Registry.register(Registry.ITEM, new Identifier("chemica", "cassiterite_ore"), new BlockItem(CASSITERITE_ORE_BLOCK, new Item.Settings().group(CHEMICA_ORES)));
     }
 
     public static void init() {
-
-        MALACHITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(2.f).build());
-        ARGENTITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(4.f).build());
-        CHALCOPYRITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(4.f).build());
-        GALENA_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(4.f).build());
-        CUPRITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(3.f).build());
-        PYRITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(4.f).build());
-        RUTILE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(5.f).build());
-        SILVER_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(4.f).build());
-        SPHALERITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(4.f).build());
         ZINCITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(3.f).build());
-        CASSITERITE_ORE_BLOCK = new OreBlock(FabricBlockSettings.of(Material.STONE).hardness(3.f).build());
 
         register();
     }
